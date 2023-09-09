3edf82a4cc0fb59a09da07f224defecdf4fb014c
Vidhi <srivastavavidhi29@gmail.com>
2023-09-09 22:05:11 +0530

    Created Firebase project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("hello");
+import {initializeApp} from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAs0mBmSPMzwWxnJPFZvbB0XByH7uthSeM",
+    authDomain: "learning-firebase-d3454.firebaseapp.com",
+    projectId: "learning-firebase-d3454",
+    storageBucket: "learning-firebase-d3454.appspot.com",
+    messagingSenderId: "654183269849",
+    appId: "1:654183269849:web:61175fdf63aae013b205c8"
+};
+
+initializeApp(firebaseConfig)
